39d257fab35fd5fa496634b80e06856981feb87b
Milad Khakpour <milad.khakpour@gmail.com>
2015-08-06 14:21:20 +0200

    Geoits project created

---
+++ functional_tests/all_users.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import unittest
+import warnings
+ 
+class NewVisitorTest(unittest.TestCase):
+ 
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+ 
+    def tearDown(self):
+        self.browser.quit()
+ 
+    def test_it_worked(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Welcome to Django', self.browser.title)
+ 
+if __name__ == '__main__':
+    with warnings.catch_warnings(record=True):
+        unittest.main()
+   # unittest.main(warnings='ignore')
