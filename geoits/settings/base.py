aa9bbc3f817de6b583d6cd904cdb5aa8de5df000
Milad Khakpour <milad.khakpour@gmail.com>
2015-08-06 16:45:54 +0200

    Add static files and templates

--- geoits/settings/base.py
+++ geoits/settings/base.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'geoits.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,3 +109,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
