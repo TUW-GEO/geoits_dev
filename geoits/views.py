7382692e999dfca9020df27f64b3fb3d77507acd
Milad Khakpour <milad.khakpour@gmail.com>
2015-08-10 09:17:29 +0200

    Edit robots anf humans for search engine

--- geoits/views.py
+++ geoits/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "geoits/index.html", {})
+
+def home_files(request, filename):
+    return render(request, filename, {}, content_type="text/plain")
