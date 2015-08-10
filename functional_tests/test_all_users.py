d47ed1d6439f809b9d6d286059606a76a591b8fc
Milad Khakpour <milad.khakpour@gmail.com>
2015-08-10 09:18:48 +0200

    Install coverage

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -23,4 +23,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser.get(self.get_full_url("home"))
         h1 = self.browser.find_element_by_tag_name("h1")
         self.assertEqual(h1.value_of_css_property("color"), "rgba(192, 192, 192, 0.3)")
- 
+
+    def test_home_files(self):
+        self.browser.get(self.live_server_url + "/robots.txt")
+        self.assertNotIn("Not Found", self.browser.title)
+        self.browser.get(self.live_server_url + "/humans.txt")
+        self.assertNotIn("Not Found", self.browser.title)
