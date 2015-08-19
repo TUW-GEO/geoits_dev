d27b37bd1a070534c0aadcfedb4b62fa46eed789
Milad Khakpour <milad.khakpour@gmail.com>
2015-08-19 13:42:44 +0200

    Add geographic admin OSM

--- geoits/apps/gmap/admin.py
+++ geoits/apps/gmap/admin.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.gis import admin as gisadmin
+from geoits.apps.gmap import models
+from geoits.apps.gmap.forms import GMapForm
 
 # Register your models here.
+
+
+@admin.register(models.GMap)
+class MapAdmin(gisadmin.OSMGeoAdmin):
+
+    list_display = ("timestamp", "geom")
+    form = GMapForm
